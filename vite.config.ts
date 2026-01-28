bba331064834260a1180d8aa0297ccf87bc897ef
Gabriel Credico <me@gabrielcredico.de>
2026-01-28 12:42:04 +0100

    Remove Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const host = process.env.TAURI_DEV_HOST
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -15,19 +13,9 @@ export default defineConfig({
     // Tauri expects a fixed port, fail if that port is not available
     strictPort: true,
     // If the host Tauri is expecting is set, use it
-    host: host || false,
     port: 5173,
   },
 
   // Env variables starting with TAURI_ will be exposed to the client
-  envPrefix: ['VITE_', 'TAURI_'],
-
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
-    // Don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // Produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
+  envPrefix: ['VITE_'],
 })
